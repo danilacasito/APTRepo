b2a0f1e92cbcd8812f8258bb9c19db7fe6639d7e
Daniel <danilacasito8@gmail.com>
2022-04-10 12:00:02 +0200

    update

--- update.sh
+++ update.sh
@@ -1,8 +1,13 @@
 export EMAIL="danilacasito8@gmail.com"
 export GITHUB_USERNAME="danilacasito"
+
 dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
+
 apt-ftparchive release . > Release
 gpg --default-key "${EMAIL}" --abs -o - Release > Release.gpg
 gpg --default-key "${EMAIL}" --clearsign -o - Release > InRelease
 
+git add .
+git commit -m "update"
+git push
